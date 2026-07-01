2832376f4e4a1c1c5c754e666c84115ee6eabeaa
Suvam Ghosh <suvam25@kgpian.iitkgp.ac.in>
2026-07-01 10:11:32 +0530

    some chnages

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -10,11 +10,7 @@ if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
 }
 
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
+
 let cached = global.mongoose;
 
 if (!cached) {
